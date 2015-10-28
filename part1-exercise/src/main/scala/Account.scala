34fa3f192a70f60a8fb91ab8f60cef5f082e26a1
Viktor <vikfand@gmail.com>
2015-10-28 13:51:04 +0100

    Add a few comments

--- part1-exercise/src/main/scala/Account.scala
+++ part1-exercise/src/main/scala/Account.scala
@@ -6,6 +6,7 @@ class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
   var balance: Double = initialBalance
 
   def withdraw(amount: Double): Unit = {
+    // this.synchronized locks this object so that other threads cannot access it
     this.synchronized {
       if (amount < 0) throw new IllegalAmountException(s"Cannot withdraw negative amount $amount")
       else if (this.balance - amount < 0) {
@@ -17,6 +18,7 @@ class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
   }
 
   def deposit(amount: Double): Unit = {
+    // this.synchronized locks this object so that other threads cannot access it
     this.synchronized {
       if (amount < 0) throw new IllegalAmountException(s"Cannot deposit negative amount $amount")
       else this.balance += amount
