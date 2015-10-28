dec8813dd02e33222a42ca5634172975840f310d
Viktor <vikfand@gmail.com>
2015-10-28 13:48:20 +0100

    Implement methods. Tests run

--- part1-exercise/src/main/scala/Bank.scala
+++ part1-exercise/src/main/scala/Bank.scala
@@ -1,14 +1,20 @@
-
+import exceptions.IllegalAmountException
+import exceptions.NoSufficientFundsException
 
 object Bank {
   
   private var idCounter: Int = 0
   
-  def transaction(from: Account, to: Account, amount: Double): Unit = ??? //Implement
+  def transaction(from: Account, to: Account, amount: Double): Unit = {
+    if (amount < 0) throw new IllegalAmountException(s"Cannot transfer negative amount $amount")
+    else {
+      from.withdraw(amount)
+      to.deposit(amount)
+    }
+  }
   
   def getUniqueId: Int = {
     idCounter += 1
     idCounter
   }
-  
 }
