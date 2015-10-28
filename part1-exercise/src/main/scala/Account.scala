3a81fdd651baca4244f917c667463c0185dd1fdf
Viktor <vikfand@gmail.com>
2015-10-28 12:35:11 +0100

    Inital commit

---
+++ part1-exercise/src/main/scala/Account.scala
@@ -0,0 +1,10 @@
+
+class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
+  
+  def withdraw(amount: Double): Unit = ??? //Implement
+  
+  def deposit(amount: Double): Unit = ??? //Implement
+  
+  def getBalanceAmount: Double = ??? //Implement
+
+}
---
+++ part1-exercise/src/main/scala/Bank.scala
@@ -0,0 +1,14 @@
+
+
+object Bank {
+  
+  private var idCounter: Int = 0
+  
+  def transaction(from: Account, to: Account, amount: Double): Unit = ??? //Implement
+  
+  def getUniqueId: Int = {
+    idCounter += 1
+    idCounter
+  }
+  
+}
