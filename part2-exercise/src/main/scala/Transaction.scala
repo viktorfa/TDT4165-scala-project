fc3a41a328b6d7b3bad989ad7834e2c36c078314
unknown <ahmedoa@stud.ntnu.no>
2015-11-08 14:17:00 +0100

    Some queue methods + transaction_run

--- part2-exercise/src/main/scala/Transaction.scala
+++ part2-exercise/src/main/scala/Transaction.scala
@@ -1,24 +1,34 @@
 
 import exceptions._
 import scala.collection.mutable
+import scala.collection.mutable.Queue
 
 object TransactionStatus extends Enumeration {
   val SUCCESS, PENDING, FAILED = Value
 }
 
 class TransactionQueue {
-  
+
+  val transactions = Queue[Transaction]
+
   // Remove and return the first element from the queue
-  def pop: Transaction = ???
+  def pop: Transaction = {
+
+    transactions.dequeue()
+  }
 
   // Return whether the queue is empty
   def isEmpty: Boolean = ???
 
   // Add new element to the back of the queue
-  def push(t: Transaction): Unit = ???
+  def push(t: Transaction): Unit = {
+    transactions.enqueue(t)
+  }
 
   // Return the first element from the queue without removing it
-  def peek: Transaction = ???
+  def peek: Transaction = {
+    
+  }
 
   // Return an iterator to allow you to iterate over the queue
   def iterator: Iterator[Transaction] = ???
@@ -34,22 +44,36 @@ class Transaction(val transactionsQueue: TransactionQueue,
   var status: TransactionStatus.Value = TransactionStatus.PENDING
 
   override def run: Unit = {
-    
-    def doTransaction() = {
-      from withdraw amount
-      to deposit amount
-    }
-    
-    if (from.uid < to.uid) from synchronized {
-      to synchronized {
-        doTransaction
+
+
+      def doTransaction() = {
+        from withdraw amount
+        to deposit amount
       }
-    } else to synchronized {
-      from synchronized {
-        doTransaction
+
+    //Unsure on how from.uid and to.uid will change during each iteration
+
+    for(i <- 1 to allowedAttemps) {
+
+      if (from.uid < to.uid) from synchronized {
+        to synchronized {
+          doTransaction
+        }
+      } else to synchronized {
+        from synchronized {
+          doTransaction
+        }
       }
+
     }
-    
+
+    //after "allowedattempts" number of iteration, if status doesnt reach SUCCESS it gets labelled as a failure
+
+    if(status != TransactionStatus.SUCCESS){
+      status = TransactionStatus.FAILED
+    }
+
+
     //Extend this method to satisfy new requirements.
 
   }
