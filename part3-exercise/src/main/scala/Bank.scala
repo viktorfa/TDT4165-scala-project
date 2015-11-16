a2d46b3dc24db041396989dfaed6ecab5e84e47b
Viktor <vikfand@gmail.com>
2015-11-16 14:01:22 +0100

    Add a few comments

--- part3-exercise/src/main/scala/Bank.scala
+++ part3-exercise/src/main/scala/Bank.scala
@@ -22,11 +22,13 @@ class Bank(val bankId: String) extends Actor {
 
   def findAccount(accountId: String): Option[ActorRef] = {
     // Use BankManager to look up an account with ID accountId
+    // An option is either None or the type it is supposed to be, here we just cast it
     Option[ActorRef](BankManager.findAccount(this.bankId, accountId))
   }
 
   def findOtherBank(bankId: String): Option[ActorRef] = {
     // Use BankManager to look up a different bank with ID bankId
+    // An option is either None or the type it is supposed to be, here we just cast it
     Option[ActorRef](BankManager.findBank(bankId))
   }
 
@@ -47,9 +49,10 @@ class Bank(val bankId: String) extends Actor {
 
   def processTransaction(t: Transaction): Unit = {
     implicit val timeout = new Timeout(5 seconds)
+    // If the transaction is from an external bank and the destination account belongs to this bank, it must be
+    // treated as an internal transaction further down
     val isFromExternal = t.to.length > 4 && t.to.substring(0, 4).equals(this.bankId)
     val isInternal = t.to.length <= 4
-
 
     val toBankId = if (isInternal) bankId else t.to.substring(0, 4)
     val toAccountId = if (isInternal) t.to else t.to.substring(4)
@@ -58,7 +61,7 @@ class Bank(val bankId: String) extends Actor {
 
     // This method should forward Transaction t to an account or another bank, depending on the "to"-address.
     // HINT: Make use of the variables that have been defined above.
-    if (isInternal || isFromExternal) {
+    if (isInternal || isFromExternal) { // This is where we treat an external transaction as an internal
       findAccount(toAccountId).get ! t
     } else {
       findOtherBank(toBankId).get ! t
